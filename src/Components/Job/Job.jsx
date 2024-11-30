8e514b11672e4328bd1d2e07805bbd835786c4aa
captainboogey <anik35-700@diu.edu.bd>
2024-12-01 02:56:07 +0600

    adding show all

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { IoLocationOutline } from "react-icons/io5";
+import { LuCircleDollarSign } from "react-icons/lu";
 
 const Job = ({ job }) => {
   const { logo,job_title,company_name,remote_or_onsite,location,job_type,salary } = job;
@@ -17,6 +19,10 @@ const Job = ({ job }) => {
           <div className="flex gap-2">
             <button className="text-[#7E90FE] py-2 px-5 font-extrabold border border-[#7E90FE] rounded">{remote_or_onsite}</button>
             <button className="text-[#7E90FE] py-2 px-5 font-extrabold border border-[#7E90FE] rounded">{job_type}</button>
+          </div>
+          <div className="mt-4 flex gap-2 items-center">
+            <h2 className="flex gap-2"><IoLocationOutline className="text-2xl"></IoLocationOutline>{location}</h2>
+            <h2  className="flex gap-2"><LuCircleDollarSign className="text-2xl"></LuCircleDollarSign>{salary}</h2>
           </div>
           <div className="card-actions ">
             <button className="btn btn-primary">View Details</button>
