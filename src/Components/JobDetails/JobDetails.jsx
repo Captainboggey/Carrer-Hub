90e7423f800e03e20a92888cdffa7870cf96fe93
captainboogey <anik35-700@diu.edu.bd>
2024-12-01 13:42:54 +0600

    adding applied jobs

--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { saveJobApplication } from '../../Utility/localStorage';
 
 const JobDetails = () => {
     const jobs = useLoaderData();
     const {id} = useParams();
     const job = jobs.find(job => job.id == id)
     console.log(job)
+
+    const handleApplyJob=()=>{
+        saveJobApplication(id)
+        toast('you have applied successfully')
+    }
+
     return (
         <div>
             
@@ -17,9 +26,9 @@ const JobDetails = () => {
                 </div>
                     <div className='border'>
                         <h2 className='text-2xl'>Side things</h2>
-                       <button className='btn btn-primary w-full'> Apply Now</button>
+                       <button onClick={handleApplyJob} className='btn btn-primary w-full'> Apply Now</button>
                     </div>
-
+                    <ToastContainer />
                 
 
             </div>
